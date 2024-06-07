025ff137b436c426363f9652b79a5db4442a7fb2
salwan <mark123.mm51@gmail.com>
2024-06-07 14:36:05 +0300

    added post put patch and delete functions to our APIView

--- src/profiles_api_project/profiles_api/views.py
+++ src/profiles_api_project/profiles_api/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """ test api view """
-
+    serializer_class = serializers.HelloSerializer
     def get(self, request,format=None):
 
         an_apiview =[
@@ -17,3 +19,31 @@ class HelloApiView(APIView):
         'mapped manually to URLs',
         ]
         return Response({'message':'Hello','an_apiview':an_apiview})
+    def post(self,request):
+        """create a hellow message with our name"""
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+
+            message = 'Hellow {0}'.format(name)
+
+            return Response({'message': message})
+        else :
+            return Response(
+                serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def put(self,resquest,pk=None):
+        """handels updating an opject"""
+        return Response({'method':'put'})
+
+    def patch(self,resquest,pk=None):
+        """patch request, only updates the fields provided in the request"""
+        return Response({'method': 'patch'})
+
+
+    def delete(self, resquest, pk=None):
+        """deletes an opject"""
+        return Response({'method': 'deldete'})
+
+
+
